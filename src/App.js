3ef9b2f41c9063a80a27749813fbab23f08bf05e
Thomas Moser <mail@thomasmoser.ch>
2017-12-08 12:24:18 +0100

    feat: Added idb from npm and implemented simple db for saving requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import idb from 'idb'
 
 class App extends Component {
   constructor(){
@@ -11,25 +12,61 @@ class App extends Component {
 
   componentDidMount(){
     const url=`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.state.symbol}&interval=60min&outputsize=compact&apikey=${process.env.REACT_APP_GRAPHQL_URI}`
-    fetch(url, {mode: 'cors'})
-    .then((response) => {
-      if (!response.ok) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      } else {
-        return response.json()
+    //Get Data from IndexedDB
+    //Let's assume for now that the data is never updated (TODO: Do this when data updates)
+    idb.open('stock-price-db', 1, (upgradeDB) => {
+      //This function is only performed, if there is no indexedDB with the name 'stock-price-db'
+      //Here I'll create a key-value-store
+      var keyValStore = upgradeDB.createObjectStore('keyval')
+      //Here I'll add a value to my key-value-store
+      //Notice that .put() takes first the value and then the key
+      keyValStore.put(undefined, 'shareprice')
+    })
+    .then((db) => {
+      //get Data from IndexedDB
+      var tx = db.transaction('keyval')
+      var keyValStore = tx.objectStore('keyval')
+      return keyValStore.get('shareprices')
+    })
+    .then((data) => {
+      console.log(data);
+      //Check if DB contains data i need
+      if(data){
+        putDataInState(data)
+      }else{
+        getDataFromAPI(url, db)
       }
     })
-    .then(json => {
-      if(json){
-        console.log(Object.keys(json["Time Series (60min)"]));
-        const mostRecentDateTime = Object.keys(json["Time Series (60min)"])[0]
-        const closingPrice = json["Time Series (60min)"][mostRecentDateTime]["4. close"]
-        this.setState({shareprice: closingPrice})
-      }
-    })
-    .catch(e => {console.log(e)})
+    .catch(e => console.log(e))
+
+    const putDataInState = (obj) => {
+      const mostRecentDateTime = Object.keys(obj["Time Series (60min)"])[0]
+      const closingPrice = obj["Time Series (60min)"][mostRecentDateTime]["4. close"]
+      this.setState({shareprice: closingPrice})
+    }
+
+    const getDataFromAPI = (url) => {
+      fetch(url, {mode: 'cors'})
+      .then((response) => {
+        if (!response.ok) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        } else {
+          return response.json()
+        }
+      })
+      .then(json => {
+        //getDataInState(json);
+        var dbPromise =  idb.open('stock-price-db', 1, (upgradeDB) => {})
+        dbPromise.then( db => {
+          var tx = db.transaction('keyval','readwrite')
+          var keyValStore = tx.objectStore('keyval')
+            keyValStore.put(json, 'shareprices')
+        })
+        putDataInState(json)
+      })
+      .catch(e => {console.log(e)})
+    }
   }
   render() {
     return (
