a093ade1839ccfd065f5ab16d2410c0d17b8a6cd
Thomas Moser <mail@thomasmoser.ch>
2018-01-09 08:50:03 +0100

    refactor: Updated test for piechart

--- src/components/molecules/piechart.test.js
+++ src/components/molecules/piechart.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PieChartWithData from './piechart';
 import renderer from 'react-test-renderer'
-import { portfolio } from '../../data/data'
+import { portfolio, currency, holdingsWithMarketPrice, shareValue } from '../../data/data'
 
 it('Shows portfolio as pie chart', () => {
   const component = renderer.create(
-    <PieChartWithData portfolio={portfolio}/>
+    <PieChartWithData portfolio={portfolio} currency={currency} holdingsWithMarketPrice={holdingsWithMarketPrice} shareValue={shareValue} />
       );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
