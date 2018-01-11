fcef4e34f943e095c1271e310df01b14c026a2ec
Thomas Moser <mail@thomasmoser.ch>
2018-01-11 14:45:43 +0100

    feat: Added basic test for TableWithHoldings Component

---
+++ src/components/molecules/tablewithholdings.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TableWithHoldings from './tablewithholdings';
+import renderer from 'react-test-renderer'
+import { portfolio, currency, holdingsWithMarketPrice, shareValue } from '../../data/data'
+
+it('Shows portfolio as pie chart', () => {
+  const component = renderer.create(
+    <TableWithHoldings
+      portfolioSelection="Cash"
+      color="blue"
+      portfolio={portfolio}
+      currency={currency}
+    />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
