8c8ed11eec229f9ab5159b4b3c18d0c6a5e9747b
Thomas Moser <mail@thomasmoser.ch>
2018-01-16 10:21:17 +0100

    test: Updated e2e test for remove investment

--- test/specs/app/test.js
+++ test/specs/app/test.js
@@ -7,13 +7,14 @@ describe('Personal Portfolio App', () => {
     expect(actualTitle).to.eql('Personal Portfolio App')
   })
 
-  it('Should allow me to remove an investment', () => {
+  it('Should remove investment from table', () => {
     browser.url('http://localhost:3000/')
     browser.click('path:first-of-type')
     browser.waitForExist('table');
     const tableRowToBeRemoved = browser.element('table:first-of-type tbody tr:first-child')
     const actual = tableRowToBeRemoved.element('button')
     tableRowToBeRemoved.click('button:first-element')
+    browser.waitForExist('#notification');
     tableRows = browser.elements('table:first-of-type tbody tr')
     expect(tableRows).to.not.include(tableRowToBeRemoved)
   })
