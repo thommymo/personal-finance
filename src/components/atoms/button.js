49adfc8acf786209f9e9184179368fbbdc16fb2f
Thomas Moser <mail@thomasmoser.ch>
2018-01-18 09:13:29 +0100

    feat: Updated styling for buttons

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  width:100%;
-  background-color: white;
+  min-width:40%;
+  margin:0 5px;
+  background-color: rgba(245,245,245,0.8);
   color: black;
-  border: 2px solid black;
-  padding: 8px;
+  border: 2px solid white;
+  padding: 5px;
   -webkit-transition-duration: 0.4s; /* Safari */
   transition-duration: 0.4s;
   align-self: center;
